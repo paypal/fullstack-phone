8dfffaa378ac7c9baf712c5e227af9200e7281e2
Nathan Hammond <nathan@nathanhammond.com>
2014-04-20 02:09:38 -0400

    Automated build, better version for library.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CWD=`pwd`
+
+if [ ! -d "vendor" ]; then
+  mkdir vendor
+fi
+
+# Install or update the dependencies.
+LIBRARIES=(libphonenumber closure-library closure-compiler closure-linter python-gflags)
+for LIBRARY in "${LIBRARIES[@]}"
+do
+  if [ ! -d "vendor/$LIBRARY" ]; then
+    svn checkout http://$LIBRARY.googlecode.com/svn/trunk/ vendor/$LIBRARY
+  else
+    svn update vendor/$LIBRARY
+  fi
+done
+
+# Build closure-compiler
+ant -f vendor/closure-compiler/build.xml
+
+# Build libphonenumber
+ant -f vendor/libphonenumber/javascript/build.xml compile-demo
