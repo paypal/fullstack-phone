885381066e61faddcac97c3bd7d04984797df7ff
Nathan Hammond <github.com@nathanhammond.com>
2015-09-18 10:21:41 -0700

    Remove Google Code, better parameterization.

--- build.sh
+++ build.sh
@@ -6,38 +6,17 @@ if [ ! -d "vendor" ]; then
   mkdir vendor
 fi
 
-# Install or update the SVN dependencies.
-LIBRARIES=(closure-linter python-gflags)
-for LIBRARY in "${LIBRARIES[@]}"
-do
-  if [ ! -d "vendor/$LIBRARY" ]; then
-    svn checkout http://$LIBRARY.googlecode.com/svn/trunk/ vendor/$LIBRARY
-  else
-    svn update vendor/$LIBRARY
-  fi
-done
-
 # Install or update the git dependencies.
-LIBRARIES=(closure-library closure-compiler)
+LIBRARIES=(google/closure-linter gflags/python-gflags google/closure-library google/closure-compiler googlei18n/libphonenumber)
 for LIBRARY in "${LIBRARIES[@]}"
 do
-  if [ ! -d "vendor/$LIBRARY" ]; then
-    git clone https://github.com/google/$LIBRARY vendor/$LIBRARY
+  SPLIT=(${LIBRARY//\// })
+  ORGANIZATION=${SPLIT[0]}
+  PROJECT=${SPLIT[1]}
+  if [ ! -d "vendor/$PROJECT" ]; then
+    git clone https://github.com/$ORGANIZATION/$PROJECT vendor/$PROJECT
   else
-    cd vendor/$LIBRARY
-    git pull
-    cd ../..
-  fi
-done
-
-# Install or update the git dependencies.
-LIBRARIES=(libphonenumber)
-for LIBRARY in "${LIBRARIES[@]}"
-do
-  if [ ! -d "vendor/$LIBRARY" ]; then
-    git clone https://github.com/googlei18n/$LIBRARY vendor/$LIBRARY
-  else
-    cd vendor/$LIBRARY
+    cd vendor/$PROJECT
     git pull
     cd ../..
   fi
