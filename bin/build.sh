174374b49d57759b687e73fa6f88a8893b18d7bb
Rafael Xavier de Souza <rxaviers@gmail.com>
2019-11-08 15:00:24 -0300

    Feat: Node.js support for client code (#11)
    Global scope could be either this, self, or global* (the added support).

--- bin/build.sh
+++ bin/build.sh
@@ -29,4 +29,10 @@ curl \
   --output client/index.js \
   https://closure-compiler.appspot.com/compile
 
+# Add support for Node.js
+grep '^var aa=this' client/index.js >/dev/null || echo "WARNING: Failed to apply Node.js support, please check bin/build.sh"
+sed -ie \
+  's/^var aa=this||self/var aa;if(void 0!==this)aa=this;else if("undefined"!=typeof global)aa=global;else{if("undefined"==typeof self)throw new Error("Can'"'"'t find global scope");aa=self}/g' \
+  client/index.js
+
 echo "Done."
